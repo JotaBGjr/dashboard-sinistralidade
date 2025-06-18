61985c51f6f8690e0a96dd34103133d04548d7a8
JotaBGjr <jorgebarretog00@outlook.com.br>
2025-06-18 17:03:50 -0300

    imagem_hapvida.jpg

--- 0_🏠_Home.py
+++ 0_🏠_Home.py
@@ -146,7 +146,7 @@ def img_operadora(etapa_nome):
     imagens = {
         "Bradesco": "pages/imagens/imagem_bradesco.jpg",
         "Omint": "pages/imagens/imagem_omint.jpg",
-        "Hapvida": "pages/imagens/imagem_hapvida.png"
+        "Hapvida": "pages/imagens/imagem_hapvida.jpg"
         # Adicione outras operadoras aqui
     }
     for op in imagens:
