02b5edc2050ba90d5a70cec7d1cd04b93e6ebfc0
JotaBGjr <jorgebarretog00@outlook.com.br>
2025-06-26 16:23:40 -0300

    login refatorado

--- login.py
+++ login.py
@@ -3,30 +3,36 @@ import streamlit_authenticator as stauth
 import os
 import base64
 
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-        return base64.b64encode(data).decode()
+def get_base64_of_bin_file(bin_file_path):
+    """Converte arquivo binário em string base64."""
+    with open(bin_file_path, 'rb') as f:
+        return base64.b64encode(f.read()).decode()
 
-# CSS
-    st.markdown(f"""
+def load_image_base64(image_filename):
+    """Carrega imagem e retorna como base64."""
+    image_path = os.path.join(os.getcwd(), "pages", "imagens", image_filename)
+    return get_base64_of_bin_file(image_path)
+
+def apply_custom_styles():
+    """Aplica CSS customizado à interface do Streamlit."""
+    st.markdown("""
         <style>
-            html, body, .stApp {{
+            html, body, .stApp {
                 background-color: black !important;
-            }}
+            }
 
-            #MainMenu, header, footer {{visibility: hidden;}}
+            #MainMenu, header, footer {visibility: hidden;}
 
-            .logo-img {{
+            .logo-img {
                 display: block;
                 margin-left: auto;
                 margin-right: auto;
                 width: 700px;
                 margin-top: 1px;
                 margin-bottom: 3px;
-            }}
+            }
 
-            .login-box {{
+            .login-box {
                 background-color: rgba(0, 0, 0, 0.6);
                 padding: 3rem;
                 border-radius: 1rem;
@@ -34,34 +40,17 @@ def get_base64_of_bin_file(bin_file):
                 color: white;
                 max-width: 400px;
                 margin: auto;
-            }}
+            }
 
-            input {{
+            input {
                 color: black !important;
-            }}
+            }
         </style>
     """, unsafe_allow_html=True)
 
-    # Imagem
-    st.markdown(
-        f'<img class="logo-img" src="data:image/png;base64,{image_base64}">',
-        unsafe_allow_html=True
-    )
-
-
-
-
-
-
-def tela_login():
-    # Configuração de hash e imagem
+def configurar_autenticacao():
+    """Cria e retorna objeto de autenticação."""
     hashed_password = '$2b$12$T5NFaY4UIFtb03wtXKryNu6wFlun6pQt4L/aKxa2eidzixAaQ1tLi'
-    image_path = os.path.join(os.getcwd(), "pages", "imagens", "imagem_aggrega.png")
-    image_base64 = get_base64_of_bin_file(image_path)
-
-    
-
-    # Configuração do authenticator
     config = {
         'credentials': {
             'usernames': {
@@ -80,14 +69,24 @@ def tela_login():
             'emails': []
         }
     }
-
-    authenticator = stauth.Authenticate(
+    return stauth.Authenticate(
         config['credentials'],
         config['cookie']['name'],
         config['cookie']['key'],
         config['cookie']['expiry_days']
     )
 
+def tela_login():
+    """Renderiza tela de login e retorna estado de autenticação."""
+    image_base64 = load_image_base64("imagem_aggrega.png")
+    apply_custom_styles()
+
+    st.markdown(
+        f'<img class="logo-img" src="data:image/png;base64,{image_base64}">',
+        unsafe_allow_html=True
+    )
+
+    authenticator = configurar_autenticacao()
     login_result = authenticator.login("Login", "main")
 
     if login_result is not None:
@@ -99,8 +98,8 @@ def tela_login():
             st.error("Usuário ou senha incorretos.")
         else:
             st.info("Por favor, faça login.")
-        
+
         return name, auth_status, username
-    else:
-        st.info("Por favor, faça login.")
-        return None, None, None
+
+    st.info("Por favor, faça login.")
+    return None, None, None
